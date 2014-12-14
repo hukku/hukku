7548573f17faa30aba87814945e208107dcc96c7
tohosaku <ny@cosmichorror.org>
2014-12-14 14:01:43 +0900

    ver.0.0.2 set app logging

--- lib/hukku/app.rb
+++ lib/hukku/app.rb
@@ -3,6 +3,8 @@ require "sinatra/base"
 require "sinatra/config_file"
 require "json/pure"
 require "systemu"
+require "logger"
+require "fileutils"
 
 module Hukku
   class App < Sinatra::Base
@@ -16,6 +18,12 @@ module Hukku
     def self.load_settings
       config_file "config.yml"
       config_file "repo.yml"
+      unless settings.test?
+        FileUtils.mkdir_p File.join(root,"log")
+        logfile = File.join(root,'log/hukku.log')
+        FileUtils.touch logfile
+        set :logger, Logger.new(logfile, 'monthly')
+      end
     end
 
     def self.work_dir=(value)
@@ -28,23 +36,28 @@ module Hukku
       load_settings
     end
 
+
     get "/" do
       "<html></html>"
     end
 
     post "/sfjp" do
       halt 500 unless params["payload"]
+      settings.logger.info "request has payload"
 
       webhook = JSON.parse params["payload"]
       halt 500 unless webhook["repository"] && webhook["repository"]["name"]
+      settings.logger.info "request is valid webhook"
 
       repo_name = webhook["repository"]["name"]
       halt 500 unless settings.repo[repo_name]
+      settings.logger.info "repository name is registered in config file"
 
       cmd = "#{F.join(settings.gem_dir,settings.mirror_cmd)} #{settings.repo[repo_name]}"
       status, stdout, stderr = systemu cmd
 
       halt 500 unless status.success?
+      settings.logger.info "command execution is successfull"
 
       "i've received webhook!"
     end
